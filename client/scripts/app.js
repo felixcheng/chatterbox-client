0a6d95e9d5102b19bbca4faa3943eb900afb860f
Hack Reactor <hackreactor@Hacks-Mac-mini-2.local>
2014-01-20 22:09:51 -0800

    added stuff

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,58 +1,114 @@
+var roomname = "";
+var roomList = {};
 
-var addMessages = function(data, display) {
-  // debugger;
+var displayMessages = function(messages, display, roomname) {
   var context;
-  var messages = data.results;
   var message_html;
   var $message; 
   // var $display = clear_messages(display);
   var source = $("#message-template").html();
   var template = Handlebars.compile(source);
 
+  display.html("");
+
   for(var i = 0; i < messages.length; i++){
-    $message = $('<div id="messageBox" style="clear: both;"></div>');
+    $message = $('<div id="messageBox"></div>');
+    if(messages[i].text && messages[i].text.length > 5000 ||
+       (messages[i].roomname !== roomname && roomname)){
+      continue;
+    }
     context = {
       // display_name: display,
       user_name: messages[i].username,
       time: messages[i].updatedAt,
-      message: messages[i].text
+      message: encodeHTML(messages[i].text)
     };
     message_html = template(context);
     $message.html(message_html);
-    $message.prependTo($('#display'));
+    $message.appendTo($('#messages'));
+  }
+};
+
+function encodeHTML(string) {
+  string =  string || "";
+  return string.replace(/&/g, '&amp;')
+               .replace(/</g, '&lt;')
+               .replace(/>/g, '&rt;')
+               .replace(/"/g, '&quot;');
+};
+
+// TODO: clear out roomList periodically
+var buildRoomList = function(data, roomListModel, $roomListView){
+  _.each(data, function(value){
+    if (!roomListModel[value.roomname]){
+      roomListModel[value.roomname] = true;
+      $roomListView.append('<ul class="room">'+value.roomname+"</ul>");
+    }
+  })
+};
+
+var updateRoomname = function(roomname){
+  $('h3').text("You are in room: " + roomname);
+  if(roomname === 'all'){
+    roomname = undefined;
   }
 };
 
 $(document).ready(function(){
 
+  var url = document.URL;
+  var regex = new RegExp(/username=([^&]+)/g);
+  // debugger;
+  var userName = regex.exec(url)[1];
+
+  $("body").on("click", '.user', function(){
+    console.log("user clicked");
+  });
+
+  $('body').on("click", '.room', function(){
+    updateRoomname(this.textContent);
+    console.log("TEST!!");
+  });
+
+  $("#createRoomButton").on('click', function(){
+    updateRoomname($('#newRoomInput').val());
+  });
+
   $("#submitButton").on('click', function(){
-    var message = $("#message").val();
+    var message = $("#userInput").val();
     if(message === "") return;
+    // debugger;
     $.ajax({
       // always use this url
       url: 'https://api.parse.com/1/classes/chatterbox',
       type: 'POST',
-      data: JSON.stringify(message),
+      data: JSON.stringify({username: userName,
+                            text: message,
+                            roomname: "testicles"}),
       contentType: 'application/json',
       success: function (data) {
         console.log('chatterbox: Message sent');
       },
       error: function (data) {
+        debugger;
         // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to send message');
       }
     });
   });
-  
+
   setInterval(function(){
     $.ajax({
-      // always use this url
       url: 'https://api.parse.com/1/classes/chatterbox',
       type: 'GET',
-      data: {sortid: '-updatedAt'},
+      //to apply restriction on data received (eg. sort, limit), 
+      //pls create an object under data.
+      data: {order:'-createdAt'},
       contentType: 'application/json',
       success: function (data) {
-        addMessages(data, $('#display'));
+        var messages = data.results
+        displayMessages(messages, $('#messages'), roomname);
+        buildRoomList(messages, roomList, $('#rooms'));
       },
       error: function (data) {
         console.error('chatterbox: Failed to get new messages. ERR:' + data);
@@ -60,6 +116,7 @@ $(document).ready(function(){
     });
   }, 300);
 });
+
 
 
 // var extractMessages = function(data){
